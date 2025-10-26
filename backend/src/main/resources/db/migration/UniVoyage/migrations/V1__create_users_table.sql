af1936f784b020900f925dd8d1e6c657c60406d3
misterdrac <golubovicdario@gmail.com>
2025-10-26 15:07:41 +0100

    **Database integration & backend testing**
    - created univoyage PostgreSQL 18 database, added Flyway migration for users table
    - created migration files for V1 and V2 for creation of users table
    - added Flyway.toml migration configuration file
    - tested if backend fires up and it fires up, in backend dir run command to install all dependencies then fire up Spring-Boot, http://localhost:8080/ should open on login page, official Spring Boot login page

---
+++ backend/src/main/resources/db/migration/UniVoyage/migrations/V1__create_users_table.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE univoyage;
+
+\c univoyage
+
+CREATE TABLE users (
+    id                 BIGSERIAL PRIMARY KEY,
+    name               VARCHAR(100)       NOT NULL,
+    email              VARCHAR(150)       UNIQUE NOT NULL,
+    password           TEXT               NOT NULL,
+    role               VARCHAR(50)        NOT NULL DEFAULT 'user',
+
+    hobbies            TEXT[],            -- array of text values
+    languages          TEXT[],            -- array for multiple languages
+    country            VARCHAR(100),
+    visited            TEXT[],            -- array of visited countries or cities
+
+    profile_image      BYTEA,             -- binary data for the photo
+    date_of_register   TIMESTAMPTZ        NOT NULL DEFAULT now(),
+    date_of_last_signin TIMESTAMPTZ,
+
+    CONSTRAINT email_format CHECK (position('@' IN email) > 1)
+);
+
+COMMENT ON users.email IS 'User email address is unique, has to contain @ sign, that is define with unique constraint';
+COMMENT ON users.email IS 'Password must be atleast 8 characters long, contain numbers, lowercase, uppercase letters';
