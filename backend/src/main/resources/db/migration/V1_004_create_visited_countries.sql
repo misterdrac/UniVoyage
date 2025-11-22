58806b8fdcd068bf109cead45f048ac1e4fde4f1
misterdrac <golubovicdario@gmail.com>
2025-11-22 18:32:05 +0100

    - created migration for user visited countries, where user stores all of it's visited countries

---
+++ backend/src/main/resources/db/migration/V1_004_create_visited_countries.sql
@@ -0,0 +1,9 @@
+-- Stores a list of countries each user has visited
+-- Each user can visit multiple countries, and each country can be visited by multiple users
+CREATE TABLE user_visited_countries (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    country_code CHAR(3) REFERENCES countries(iso_code) ON DELETE CASCADE, -- References the 'countries' table
+    date_of_visit DATE, -- Optional: Stores when they visited (if tracking details)
+    UNIQUE (user_id, country_code)
+);
