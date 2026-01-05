1a73c754f31122678e04f728f3f088c541b1b5a6
misterdrac <golubovicdario@gmail.com>
2026-01-05 11:20:16 +0100

    Java Documentation
    - Java doc for main class

--- backend/src/main/java/com/univoyage/UniVoyageApplication.java
+++ backend/src/main/java/com/univoyage/UniVoyageApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Main application class for UniVoyage.
+ */
 @SpringBootApplication(scanBasePackages = "com.univoyage")
 @EnableJpaRepositories(basePackages = "com.univoyage")
 @EntityScan(basePackages = "com.univoyage")
